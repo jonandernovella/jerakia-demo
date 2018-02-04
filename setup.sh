9b73290acc80f4004eafd1cfbc4b8d790422ab22
Jan Ivar Beddari <code@beddari.net>
2018-02-04 11:27:35 +0100

    start templating demo

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ gpgcheck=0
 " | tee /etc/yum.repos.d/jerakia.repo
 
 yum install -y epel-release
-yum install -y jerakia unzip python-requests vim
+yum install -y jerakia unzip python-requests vim python2-pip
 cp -f /vagrant/jerakia/policy.d/default.rb /etc/jerakia/policy.d
 
 # install ansible
@@ -21,8 +21,9 @@ echo "localhost ansible_connection=local" > /etc/ansible/hosts
 mkdir -p ~/bin
 curl -LC - -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
 curl -LC - -o /tmp/packer.zip https://releases.hashicorp.com/packer/1.1.3/packer_1.1.3_linux_amd64.zip
-unzip -oq /tmp/terraform.zip -d ~/bin
-unzip -oq /tmp/packer.zip -d ~/bin
+unzip -oq /tmp/terraform.zip -d /usr/local/bin
+unzip -oq /tmp/packer.zip -d /usr/local/bin
+ansible all -m lineinfile -a "dest=/root/.bashrc line='alias packer=/usr/local/bin/packer'"
 
 echo "test data lookup without server"
 jerakia lookup port
