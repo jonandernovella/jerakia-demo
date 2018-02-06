7fe90f071b2a9be23dc8c8895d742c68f78d29fc
Jan Ivar Beddari <code@beddari.net>
2018-02-06 11:12:48 +0100

    add packer template rendering

---
+++ 3-packer/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+lookup() {
+  local namespace=$1
+  local key=$2
+  local value=$(jerakia lookup $key \
+    --namespace $namespace \
+    --metadata environment:${ENVIRONMENT:-prod} certname:${CERTNAME:-midtstilt_kobra})
+  echo $key=$value
+}
+
+jinja2 packer.json.j2 \
+  -D $(lookup aws aws_access_key) \
+  -D $(lookup aws aws_secret_key) \
+  -D $(lookup aws region) \
+  -D $(lookup aws image)
+
