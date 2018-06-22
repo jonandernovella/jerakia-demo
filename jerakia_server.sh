e7ec28d2c1f8b2ecd65d32da66dfbd8ef4b62934
Jan Ivar Beddari <code@beddari.net>
2018-06-22 15:43:42 +0200

    Spelling fix

--- jerakia_server.sh
+++ jerakia_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
-docker run -d -P -v $PWD/policy.d:/etc/jerakia/policy.d:z -v £$PWD/data:/var/lib/jerakia/data:z -p 9843:9843 -e USERID=$UID crayfishx/jerakia
+docker run -d -P -v $PWD/policy.d:/etc/jerakia/policy.d:z -v $PWD/data:/var/lib/jerakia/data:z -p 9843:9843 -e USERID=$UID crayfishx/jerakia
 
